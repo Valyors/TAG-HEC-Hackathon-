abcb9d8e446d3357eba6befe7c3123d046eacc28
Madderstone <anthony.issa@hotmail.fr>
2023-10-14 12:22:30 +0200

    wip

---
+++ FRONT/tag/components/nav.tsx
@@ -0,0 +1,15 @@
+export default function Nav() {
+  return (
+    <nav className="profile-nav">
+      <a href="pokedex.html">
+        <img className="nav-icon" src="pokedex-navbar.png" alt="" />
+      </a>
+      <a href="index.html">
+        <img className="nav-icon" src="navbar_qr-code.png" alt="" />
+      </a>
+      <a href="profil.html">
+        <img className="nav-icon" src="navbar_profil.png" alt="" />
+      </a>
+    </nav>
+  );
+}
